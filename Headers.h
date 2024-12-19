81beeea21f4f2b6c1652820b481dc286d897aa6f
Aidan Levy <168674488+AidanLevy05@users.noreply.github.com>
2024-12-19 12:38:22 -0500

    Create Headers.h

---
+++ Headers.h
@@ -0,0 +1,13 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+#endif
